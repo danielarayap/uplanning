935d71166f7c131afefdc2385708cc6cfc66f4dd
gabriel <gabrielchaperonb@gmail.com>
2019-08-16 13:54:26 -0400

    cambia puerto del servidor

--- backend/server.py
+++ backend/server.py
@@ -25,4 +25,4 @@ def home():
 
 # cuando se corre directamente, correr la aplicacion
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
