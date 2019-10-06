cf62653b72f7a1f03884ec447241a01f310f8418
mr <aege21@gmail.com>
2019-10-06 17:48:27 -0300

    modified new semester view margins

--- uplanning-app/src/components/NewSemester.js
+++ uplanning-app/src/components/NewSemester.js
@@ -18,29 +18,24 @@ export default class NewSemester extends React.Component {
   render() {
     return (
       <div>
-        <h4>Crear semestre</h4>
+        <h2>Crear semestre</h2>
 
         <form>
-        <div id="load-semester">
-          Cargar semestre
-          <br/>
-          <input type="file" name="xls-semester"/>
-        </div>
+			<div>	
+				<div id="load-semester">
+					<h4>Cargar semestre</h4>
+          			<input type="file" name="xls-semester"/>
+		        </div>
 
-        <br></br>
-
-        <div id="copy-semester">
-          Replicar semestre
-          <br/>
-          <SelectSemester/>
-        </div>
-
-        <br></br>
-
-        <div id="new-semester-buttons">
-          <input id="preview-semester" type="button" value="Previsualizar"/>  
-          <input id="save-semester" type="submit" value="Guardar"/>
-        </div>
+        		<div id="copy-semester">
+					<h4>Replicar semestre</h4>
+          			<SelectSemester/>
+		        </div>
+			</div>
+        	<div id="new-semester-buttons">
+          		<input id="preview-semester" type="button" value="Previsualizar"/>  
+	          	<input id="save-semester" type="submit" value="Guardar"/>
+    	    </div>
         </form>
       </div>
     )
