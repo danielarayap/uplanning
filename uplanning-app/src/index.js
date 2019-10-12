51c294250248875daacadc68da4ae3c4d4fe5cea
mr <aege21@gmail.com>
2019-10-12 13:23:22 -0300

    added navbar with dropdown

--- uplanning-app/src/index.js
+++ uplanning-app/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App"
 import Login from "./components/Login";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // ReactDOM.render(<Login />, document.getElementById("root"));
