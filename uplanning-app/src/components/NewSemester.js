e400b346e0a1f667fa3a0a1645f3617a0432a626
mr <aege21@gmail.com>
2019-10-13 08:54:14 -0300

    NewSemester.js modified to be responsive

--- uplanning-app/src/components/NewSemester.js
+++ uplanning-app/src/components/NewSemester.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { Alert, Button, Container, Col, Row } from "react-bootstrap";
+import { Alert, Button, Container, Col, Row, InputGroup } from "react-bootstrap";
 
 class SelectSemester extends React.Component {
-  render() {
-    return (
-      <select>
-        <option value="semester1">Otoño 2019</option>
-        <option value="semester2">Primavera 2018</option>
-        <option value="semester3">Otoño 2018</option>
-        <option value="semester4">Primavera 2017</option>
-        <option value="semester5">Otoño 2017</option>
-      </select>
-    );
-  }
+	render() {
+		return (
+			<InputGroup>
+				<select className="custom-select">
+					<option value="semester1">Otoño 2019</option>
+       	 	    	<option value="semester2">Primavera 2018</option>
+        	    	<option value="semester3">Otoño 2018</option>
+			    	<option value="semester4">Primavera 2017</option>
+        	    	<option value="semester5">Otoño 2017</option>
+	      	    </select>
+			</InputGroup>
+        );
+    }
 };
+
+
+class SelectFile extends React.Component {
+	render() {
+		return (
+			<InputGroup>
+				<div class="custom-file">
+					<input type="file" class="custom-file-input" id="upload-semester-input"/>
+					<label class="custom-file-label" for="upload-semester-input"> Escoger Archivo</label>
+				</div>
+				<InputGroup.Append>
+					<Button variant="outline-secondary bg-light">Subir</Button>
+				</InputGroup.Append>
+			</InputGroup>
+		);
+	}
+}
+
 
 export default class NewSemester extends React.Component {
 	render() {
@@ -24,14 +44,14 @@ export default class NewSemester extends React.Component {
 							<h3>Crear Semestre</h3>
 					</Col>
 				</Row>
-				<Row className="mb-4">
-					<Col xs={12} md={6}>
+				<Row className="mb-1">
+					<Col xs={12} md={6} className="mb-4">
 						<Alert id="load-semester-alert" variant="primary">
 							<h4>Cargar Semestre</h4>
-							<input type="file" name="xls-semester"/>
+							<SelectFile/>
 						</Alert>
 					</Col>
-					<Col xs={12} md={6}>
+					<Col xs={12} md={6} className="mb-4">
 						<Alert id="replicate-semester-alert" variant="primary">
 							<h4>Replicar Semestre</h4>
 							<SelectSemester/>
