d876dd46519e426591c3610df0fb0cd6efc293bc
mr <aege21@gmail.com>
2019-10-13 12:55:46 -0300

    modified index.css and index.js

--- uplanning-app/src/index.js
+++ uplanning-app/src/index.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App"
-import Login from "./components/Login";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
-// ReactDOM.render(<Login />, document.getElementById("root"));
