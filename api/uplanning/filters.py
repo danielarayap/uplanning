771658fe2d846e308435dabd0fd4b4cf65939287
gabriel <gabrielchaperonb@gmail.com>
2019-10-16 10:58:06 -0300

    add new filter

--- api/uplanning/filters.py
+++ api/uplanning/filters.py
@@ -1,19 +1,31 @@
 from django_filters import rest_framework as filters
-from uplanning.models import Evaluation
+from uplanning.models import Evaluation, Course
 
 
 class EvaluationFilter(filters.FilterSet):
-    year = filters.NumberFilter(
-        # method='get_year',
-        field_name='course__semester__year'
-        )
+    """
+    Notar los dobles guiones en los field_name, esta es la sintaxis de
+    django para "seguir" las relaciones
+    """
+    year = filters.NumberFilter(field_name='course__semester__year')
     period = filters.NumberFilter(field_name='course__semester__period')
+    semester = filters.NumberFilter(field_name='course__semester')
+    course_code = filters.CharFilter(field_name='course__ramo__code')
+    course_section = filter.NumberFilter(field_name='course__section')
 
     class Meta():
         model = Evaluation
         fields = "__all__"
 
-    # def get_year(self, queryset, field_name, value, ):
-    #     if value:
-    #         return queryset.filter(course__semester__year=value)
-    #     return queryset
+
+class CourseFilter(filters.FilterSet):
+    """
+    Misma nota de arriba
+    """
+    year = filters.NumberFilter(field_name='course__semester__year')
+    period = filters.NumberFilter(field_name='course__semester__period')
+    semester = filters.NumberFilter(field_name='course__semester')
+
+    class Meta():
+        model = Course
+        fields = "__all__"
